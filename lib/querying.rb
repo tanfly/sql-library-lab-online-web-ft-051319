26570afb11048b4ff0c1dbf11c73b0138e57b331
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 01:47:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,6 +41,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name 
   FROM characters 
-  INNER JOIN books 
+  INNER JOIN character_books 
   ON characters.book_id"
 end
