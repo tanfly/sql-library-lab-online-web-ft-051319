f3d502e76c081e21f2f8d61aeed4e4fb4899eaf6
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 01:45:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,9 +29,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species,
-  COUNT(characters.species)
-  "
+  "SELECT series.title
+   FROM series
+   INNER JOIN characters
+   ON series.id = characters.series_id
+   WHERE characters.species = 'human'
+   GROUP BY series.title
+   LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
