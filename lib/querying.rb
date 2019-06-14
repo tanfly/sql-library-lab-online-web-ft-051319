6e3d645e3f873b81aba5f56b9ec0a9b215753db9
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 01:56:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,5 +43,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM character_books
   INNER JOIN characters 
   ON character_books.character_id = characters.id
-  GROUP BY character_books.book_id;"
+  GROUP BY characters.name
+  ORDER BY ;"
 end
