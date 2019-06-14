2731761bbe4c68cb17ce08b4bed7575ad67ecb21
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 01:51:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,7 +40,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name 
-  
+  SUM(character_books.book_id)
   INNER JOIN character_books 
   ON character_books.character_id = characters.id;"
 end
