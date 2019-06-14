df27485362a97aa945cc12298f7b717abebcdb60
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 01:29:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,22 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+  FROM books 
+  INNER JOIN series WHERE books.series_id = series.id
+  GROUP BY books.year HAVING series.id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters
+   ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species,
+  COUNT(species)
+  FROM characters
+  GROUP BY species 
+  ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
