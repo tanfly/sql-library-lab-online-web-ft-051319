2c0ac2656091bff2abb32e000a5b906164403d2e
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 01:42:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors 
-  INNER JOIN series"
+  INNER JOIN series
+  ON authors.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
