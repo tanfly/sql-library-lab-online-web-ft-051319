78193f6e60b94ddf43a82f2a61e7824bac689f7c
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 01:52:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name 
+  "SELECT characters.name, 
   SUM(character_books.book_id)
   FROM character_books
   INNER JOIN characters 
